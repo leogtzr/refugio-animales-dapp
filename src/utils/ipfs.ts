25294d42f03cc3472508b6ade2443918c0d0b2dd
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2025-07-08 10:40:05 -0600

    clean / indent

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -4,15 +4,15 @@ import { Web3Storage, File } from "web3.storage";
 const WEB3STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN!;
 
 export function getWeb3StorageClient() {
-  return new Web3Storage({ token: WEB3STORAGE_TOKEN });
+    return new Web3Storage({ token: WEB3STORAGE_TOKEN });
 }
 
 export async function uploadToIPFS(files: File[]) {
-  const client = getWeb3StorageClient();
-  const cid = await client.put(files);
-  return cid;
+    const client = getWeb3StorageClient();
+    const cid = await client.put(files);
+    return cid;
 }
 
 export function getIPFSUrl(cid: string, filename: string) {
-  return `https://${cid}.ipfs.dweb.link/${filename}`;
+    return `https://${cid}.ipfs.dweb.link/${filename}`;
 }
